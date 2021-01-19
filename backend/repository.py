f5396fd97bbebd09f03edb8d2cf0b41ddfcd8a00
Pietu R <pietu_r@gmx.com>
2021-01-19 21:03:46 +0200

    backend: add repository_factory()

--- backend/repository.py
+++ backend/repository.py
@@ -1,6 +1,6 @@
 from typing import Dict
 from typing_extensions import TypedDict
-from components import Position, Velocity, Box2DWorld, Box2DBody
+from components import COMPONENTS, Position, Velocity, Box2DWorld, Box2DBody
 
 Repository = TypedDict('Repository', {
     'position': Dict[str, Position],
@@ -8,3 +8,17 @@ Repository = TypedDict('Repository', {
     'box2d_world': Dict[str, Box2DWorld],
     'box2d_body': Dict[str, Box2DBody],
 })
+
+
+def repository_factory():
+    columns = [component.component_name for component in COMPONENTS]
+    unique_columns = _validate_columns(columns)
+    return Repository(dict.fromkeys(unique_columns, {}))
+
+
+def _validate_columns(columns):
+    unique = set()
+    duplicates = set(x for x in columns if x in unique or unique.add(x))
+    if duplicates:
+        raise RuntimeError(f'duplicate component fields: {",".join(duplicates)}')
+    return unique
