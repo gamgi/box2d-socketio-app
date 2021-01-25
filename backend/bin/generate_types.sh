935190ca1d70217dea20dea5fd4b394056175e6b
Pietu R <pietu_r@gmx.com>
2021-01-25 19:48:56 +0200

    backend: script to compile backend types to typescript

---
+++ backend/bin/generate_types.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -euo pipefail
+
+FILE_PATTERN='*_interfaces.py'
+OUTPUT_DIR="../frontend/src/lib/generated/"
+BUILD_COMMAND="pipenv run py-ts-interfaces"
+
+echo "compiling files matching '${FILE_PATTERN}'"
+
+# compile files
+SUFFIX="${FILE_PATTERN#'*'}"
+find ./ -type f -name "$FILE_PATTERN" -exec \
+    bash -c "$BUILD_COMMAND \${0} -o $OUTPUT_DIR\${0/%${SUFFIX}/Interfaces.ts}" {} \;
+
+# rewrite them to export, capitalize and pass lint 
+find "$OUTPUT_DIR" -type f -name '*.ts' -exec \
+    sed -i \
+    -e "s/^interface/export interface/" \
+    -e "s/^    /  /" \
+    -e "1 i /* eslint-disable @typescript-eslint/array-type */" \
+    -e "1 i /* eslint-disable camelcase */" {} \;
+
+echo "done";
