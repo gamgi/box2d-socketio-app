4ea722450edcb8833f0fad543240ce5858429fa0
Pietu R <pietu_r@gmx.com>
2021-01-25 20:12:57 +0200

    frontend: ui for messages

--- frontend/src/app.ts
+++ frontend/src/app.ts
@@ -1,37 +1,56 @@
 import { Application, IResourceDictionary } from 'pixi.js';
-import { ci, Client, loadResources } from './lib';
+import { ci, Client, ClientError, loadResources } from './lib';
 import { Keyboard, Key, KeyboardContext } from './lib/keyboard';
 import { Game } from './game';
+import { Ui, MessageLevel } from './ui';
 import { RESOURCES } from './constants';
 
 export class App {
   private client: Client;
   private keyboard: Keyboard;
   private game: Game;
+  private ui: Ui;
   private resources: IResourceDictionary = {};
 
   constructor(app: Application, client: Client, keyboard: Keyboard, game: Game) {
     this.client = client;
     this.keyboard = keyboard;
     this.game = game;
+    this.ui = new Ui(app);
   }
 
   public async init(): Promise<void> {
+    await this.initResources();
     await this.initClient();
     this.initKeyboard();
-  }
 
-  private async initClient(reconnect = false): Promise<void> {
-    const timeout = reconnect ? 15000 : 10000;
-    await this.initResources();
-    await this.client.connect(timeout);
+    this.client.eventEmitter.on('disconnect', () => {
+      this.ui.showMessage('Connection to server lost', MessageLevel.ERROR);
+    });
   }
 
   private async initResources(): Promise<void> {
     try {
+      this.ui.showMessage('Loading');
       this.resources = await loadResources(RESOURCES);
       this.game.initResources(this.resources);
     } catch (err) {
+      this.ui.showMessage('Failed to load images', MessageLevel.ERROR);
+      throw err;
+    }
+  }
+
+  private async initClient(reconnect = false): Promise<void> {
+    const timeout = reconnect ? 15000 : 10000;
+    const message = reconnect ? 'Reconnecting' : 'Connecting';
+    try {
+      this.ui.showMessage(message);
+      await this.client.connect(timeout);
+      this.ui.clearMessage();
+    } catch (err) {
+      if (err instanceof ClientError) {
+        this.ui.showMessage(`${err.userMessage}`, MessageLevel.ERROR);
+      }
       throw err;
     }
   }
---
+++ frontend/src/ui.ts
@@ -0,0 +1,58 @@
+import { Application, Container, Graphics, Text, TextStyle, TextMetrics } from 'pixi.js';
+
+const UI_MARGIN = 200;
+const TEXT_MARGIN = 20;
+
+export enum MessageLevel {
+  INFO = 0,
+  ERROR = 1,
+}
+
+const PREFIX: Record<MessageLevel, string> = {
+  [MessageLevel.INFO]: '',
+  [MessageLevel.ERROR]: '⚠ error: ',
+};
+
+export class Ui {
+  public stage: Container = new Container();
+  private notification = new Graphics();
+  private textStyle: TextStyle;
+  private screen: { width: number; height: number };
+
+  constructor(private pixi: Application) {
+    pixi.stage.addChild(this.stage);
+    this.screen = { width: pixi.screen.width, height: pixi.screen.height };
+    this.textStyle = new TextStyle({
+      wordWrap: true,
+      wordWrapWidth: this.screen.width - UI_MARGIN * 2 - TEXT_MARGIN * 2,
+    });
+  }
+
+  public clearMessage(): void {
+    this.notification.clear();
+    this.notification.removeChildren();
+    this.stage.removeChild(this.notification);
+  }
+
+  public showMessage(message: string, level: MessageLevel = MessageLevel.INFO): void {
+    this.clearMessage();
+    const fullMessage = PREFIX[level] + message;
+    const center = { x: this.screen.width / 2, y: this.screen.height / 2 };
+    const textMetrics = TextMetrics.measureText(fullMessage, this.textStyle);
+    const { width, height } = textMetrics;
+    this.notification.beginFill(0xdeccaa);
+    this.notification.lineStyle(5, 0x8b7956);
+    this.notification.drawRect(
+      center.x - width / 2 - TEXT_MARGIN,
+      center.y - height / 2 - TEXT_MARGIN,
+      width + TEXT_MARGIN * 2,
+      height + TEXT_MARGIN * 2,
+    );
+
+    const text = new Text(fullMessage, this.textStyle);
+    text.anchor.set(0.5, 0.5);
+    text.position.set(center.x, center.y);
+    this.notification.addChild(text);
+    this.stage.addChild(this.notification);
+  }
+}
