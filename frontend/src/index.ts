d1e0595bfea4407ee5536a591f645aa3dcd3551a
Pietu R <pietu_r@gmx.com>
2021-01-26 21:11:44 +0200

    frontend: reconnect to server

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -4,16 +4,20 @@ import { HOST } from './constants';
 import { App } from './app';
 import { Game } from './game';
 
-function init(pixi: Application) {
+async function init(pixi: Application) {
   const client = new Client({ url: HOST });
   const keyboard = new Keyboard();
   const game = new Game(pixi, client);
   const app = new App(pixi, client, keyboard, game);
+  await app.init();
 
-  void app.init();
+  while (true) {
+    await app.run();
+    await app.reconnect();
+  }
 }
 
-init(
+void init(
   new Application({
     width: 800,
     height: 600,
