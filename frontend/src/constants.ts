edc2039b0221d403b7ba858e2f5a2ceb685d4923
Pietu R <pietu_r@gmx.com>
2021-01-26 17:06:52 +0200

    frontend: transform backend units to pixels

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,2 +1,5 @@
 export const HOST = 'http://localhost:5000';
 export const RESOURCES = ['./assets/missing.svg'];
+
+export const METERS_TO_PX = 100;
+export const Y_DIRECTION = -1;
--- frontend/src/game.ts
+++ frontend/src/game.ts
@@ -1,14 +1,18 @@
 import { Application, Container, IResourceDictionary } from 'pixi.js';
 import { EntityManager } from './entityManager';
 import { si, Client } from './lib';
+import { METERS_TO_PX, Y_DIRECTION } from './constants';
+
+type Vec = [number, number];
 
 export class Game {
   public stage: Container = new Container();
   private resources: IResourceDictionary = {};
-  private entityManager = new EntityManager();
+  private entityManager: EntityManager;
 
   constructor(private pixi: Application, private client: Client) {
     pixi.stage.addChild(this.stage);
+    this.entityManager = new EntityManager(pixi, this.stage);
     this.initClient();
   }
 
@@ -23,14 +27,27 @@ export class Game {
   private initClient(): void {
     this.client.eventEmitter.on('long_sync', (data: si.LongSyncDTO) => {
       for (const update of data.updates) {
+        this.clientTransformUpdateInplace(update);
         this.entityManager.updateEntity(update.id, update);
       }
     });
 
     this.client.eventEmitter.on('short_sync', (data: si.ShortSyncDTO) => {
       for (const update of data.updates) {
+        this.clientTransformUpdateInplace(update);
         this.entityManager.updateEntityShort(update.id, update);
       }
     });
   }
+
+  private clientTransformUpdateInplace(update: si.EntityData | si.ShortEntityData): void {
+    // transform coordinate system and units from MKS to local game equivalent
+    if ('position' in update) {
+      update.position = clientTransformVec(update.position as Vec);
+    }
+  }
+}
+
+function clientTransformVec(vector: Vec): Vec {
+  return [vector[0] * METERS_TO_PX, vector[1] * METERS_TO_PX * Y_DIRECTION];
 }
